4b52e374db7e47ff83c05362f9f1cdc5efec37a0
nohriter <nohriter@gmail.com>
2021-12-16 23:11:48 +0900

    + 이것이 코딩 테스트다 +1

---
+++ src/this_is_coding_test/greedy/Chap11_곱하기_혹은_더하기.java
@@ -0,0 +1,32 @@
+package this_is_coding_test.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Chap11_곱하기_혹은_더하기 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] strNumbers = br.readLine().split("");
+        int[] numbers = new int[strNumbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = Integer.parseInt(strNumbers[i]);
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (sum != 0 && numbers[i] > 1) {
+                sum *= numbers[i];
+            } else {
+                sum += numbers[i];
+            }
+        }
+
+        System.out.println(sum);
+    }
+
+}
