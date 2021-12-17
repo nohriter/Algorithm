ab5fdb1b187fd221ac5c05f3272f73388a9a84dd
nohriter <nohriter@gmail.com>
2021-12-17 23:02:04 +0900

    + 이것이 코딩 테스트다 +1

---
+++ src/this_is_coding_test/greedy/Chapter11_만들_수_없는_금액.java
@@ -0,0 +1,36 @@
+package this_is_coding_test.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Chapter11_만들_수_없는_금액 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int length = Integer.parseInt(br.readLine());
+        String[] inputNumbers = br.readLine().split(" ");
+        int[] numbers = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            numbers[i] = Integer.parseInt(inputNumbers[i]);
+        }
+
+        Arrays.sort(numbers);
+
+        int min = 1;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > min) {
+                break;
+            }
+
+            min += numbers[i];
+        }
+
+        System.out.println(min);
+    }
+
+}
