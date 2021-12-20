9f0e9b377a27f5c1d38263979e7f383142a5431a
nohri <nohriter@gmail.com>
2021-12-20 23:51:03 +0900

    이것이 코딩테스트다 +1

---
+++ src/this_is_coding_test/greedy/Chap02_상하좌우.java
@@ -0,0 +1,41 @@
+package this_is_coding_test.greedy;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Chap02_상하좌우 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+        sc.nextLine();
+        String[] move = sc.nextLine().split(" ");
+
+        int x = 1;
+        int y = 1;
+
+        for (int i = 0; i < move.length; i++) {
+            if (move[i].equals("R") && y < size + 1) {
+                y++;
+                continue;
+            }
+            if (move[i].equals("L") && y > 1) {
+                y--;
+                continue;
+            }
+            if (move[i].equals("U") && x > 1) {
+                x--;
+                continue;
+            }
+            if (move[i].equals("D") && x < size + 1) {
+                x++;
+                continue;
+            }
+        }
+
+        System.out.println(x + " " + y);
+    }
+
+}
