181d557e3b632cc016fb36643626e046cab805ec
nohri <nohriter@gmail.com>
2021-12-24 22:55:44 +0900

    이것이 코딩테스트다 +1

---
+++ src/this_is_coding_test/greedy/Chap12_문자열_재정렬.java
@@ -0,0 +1,46 @@
+package this_is_coding_test.greedy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Chap12_문자열_재정렬 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        ArrayList<String> strList = new ArrayList<>();
+        int num = 0;
+
+        String[] str = input.split("");
+
+        for (int i = 0; i < str.length; i++) {
+            if (isNumber(str[i])) {
+                num += Integer.parseInt(str[i]);
+            } else {
+                strList.add(str[i]);
+            }
+        }
+
+        Collections.sort(strList);
+        String answer = "";
+
+        for (int i = 0; i < strList.size(); i++) {
+            answer += strList.get(i);
+        }
+
+        System.out.println(answer + num);
+    }
+
+    public static boolean isNumber(String s) {
+        try {
+            Integer.parseInt(s);
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+}
