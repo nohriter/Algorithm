1855ee7e4824f88569054b91611183e308cad1fd
nohriter <nohriter@gmail.com>
2021-12-30 15:26:14 +0900

    backjoon +1

---
+++ src/backjoon/Back11399.java
@@ -0,0 +1,32 @@
+package backjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Back11399 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int answer = 0;
+
+        int n = sc.nextInt();
+
+        int[] list = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            list[i] = sc.nextInt();
+        }
+
+        Arrays.sort(list);
+
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j <= i; j++) {
+                answer += list[j];
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+}
---
+++ src/backjoon/Back2217.java
@@ -0,0 +1,37 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Back2217 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String inputRope = br.readLine();
+        int rope = Integer.parseInt(inputRope);
+
+        int[] ropes = new int[rope];
+
+        for (int i = 0; i < rope; i++) {
+            ropes[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(ropes);
+
+        int max = 0;
+
+        for (int i = ropes.length - 1; i >= 0; i--) {
+            // 최대중량 = 로프의 중량 * 로프의 수
+            ropes[i] = ropes[i] * (rope - i);
+            if (max < ropes[i]) {
+                max = ropes[i];
+            }
+        }
+
+        System.out.println(max);
+    }
+
+}
