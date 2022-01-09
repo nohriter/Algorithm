e9d40c0ad61236944ec58a63a9fa337f0feef7cc
nori <nohriter@gmail.com>
2022-01-09 23:07:34 +0900

    백준 +1

---
+++ src/backjoon/Baek2750.java
@@ -0,0 +1,26 @@
+package backjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Baek2750 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] list = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            list[i] = sc.nextInt();
+        }
+
+        Arrays.sort(list);
+
+        for (int i = 0; i < list.length; i++) {
+            System.out.println(list[i]);
+        }
+    }
+
+}
