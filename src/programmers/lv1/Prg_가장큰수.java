04c469d9c477e15be13017f757b2a55476e0e446
nori <nohriter@gmail.com>
2022-01-22 23:40:02 +0900

    프로그래머스 +1

---
+++ src/programmers/lv1/Prg_가장큰수.java
@@ -0,0 +1,31 @@
+package programmers.lv1;
+
+import java.util.*;
+
+class Prg_가장큰수 {
+    public String solution(int[] numbers) {
+        String answer = "";
+
+        String[] str = new String[numbers.length];
+
+
+        for(int i = 0; i < numbers.length; i++){
+            str[i] = String.valueOf(numbers[i]);
+        }
+
+
+        Arrays.sort(str, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                return (b+a).compareTo(a+b);
+            }
+        });
+
+
+        if (str[0].equals("0")) return "0";
+
+        for(String s: str) answer += s;
+
+        return answer;
+    }
+}
