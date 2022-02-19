45abd8cd7c64b20ad3c111af678dcd1e383dfe03
nori <nohriter@gmail.com>
2022-02-19 23:21:42 +0900

    백준 2609

---
+++ src/baekjoon/Baek2609.java
@@ -0,0 +1,31 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baek2609 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        in.close();
+
+        int d = gcd(a, b);    // 최대공약수
+
+        System.out.println(d);
+        System.out.println(a * b / d);
+
+    }
+
+    // 최대공약수 재귀 방식
+    public static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+
+        // GCD(a, b) = GCD(b, r)이므로 (r = a % b)
+        return gcd(b, a % b);
+    }
+}
