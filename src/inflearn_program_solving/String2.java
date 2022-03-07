d13ec7caa72dfd56dfe59c86af4c611983905f82
nori <nohriter@gmail.com>
2022-03-07 23:49:34 +0900

    자바 알고리즘 문제풀이 1-2

---
+++ src/inflearn_program_solving/String2.java
@@ -0,0 +1,31 @@
+package inflearn_program_solving;
+
+import java.util.Scanner;
+
+public class String2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.next();
+
+        System.out.println(solution(input));
+    }
+
+    public static String solution(String input) {
+        final char[] chars = input.toCharArray();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (char s : chars) {
+            if(Character.isUpperCase(s)) {
+                sb.append(Character.toLowerCase(s));
+            }else {
+                sb.append(Character.toUpperCase(s));
+            }
+        }
+
+        return sb.toString();
+    }
+
+}
