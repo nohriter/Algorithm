46b7dcbb6134a34ba7e94e65da020d75429251fd
nori <nohriter@gmail.com>
2022-03-08 22:49:15 +0900

    자바 알고리즘 문제풀이 1-3

---
+++ src/inflearn_program_solving/String3.java
@@ -0,0 +1,32 @@
+package inflearn_program_solving;
+
+import java.util.Scanner;
+
+public class String3 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        final String input = sc.nextLine();
+
+        final String[] split = input.split(" ");
+
+        System.out.println(solution(split));
+    }
+
+    public static String solution(String[] split) {
+        int max = split[0].length();
+
+        String answer = split[0];
+
+        for (String s : split) {
+            if(s.length() > max) {
+                max = s.length();
+                answer = s;
+            }
+        }
+
+        return answer;
+    }
+
+}
