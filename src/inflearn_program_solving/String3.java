890fb918f29d5712a4837a35023107ebed0740c5
nori <nohriter@gmail.com>
2022-03-09 23:54:16 +0900

    백준 7568

--- src/inflearn_program_solving/String3.java
+++ src/inflearn_program_solving/String3.java
@@ -3,7 +3,7 @@ package inflearn_program_solving;
 import java.util.Scanner;
 
 public class String3 {
-
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
