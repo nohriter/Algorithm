87f059b9bd20390f575e13cffbb071f628c196fd
nori <nohriter@gmail.com>
2022-03-20 23:15:01 +0900

    백준 1436

---
+++ src/baekjoon/Baek1436.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baek1436 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+
+        int num = 666;
+        int count = 1;
+
+        while (count != N) {
+            num++;
+            if (String.valueOf(num).contains("666")) {
+                count++;
+            }
+        }
+        System.out.println(num);
+    }
+}
