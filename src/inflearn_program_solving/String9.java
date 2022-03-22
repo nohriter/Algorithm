85afd0d1a34a74627d0b86de19b1cf5bd5bf6114
nori <nohriter@gmail.com>
2022-03-22 23:22:05 +0900

    자바 알고리즘 문제풀이 1-8

--- src/inflearn_program_solving/String9.java
+++ src/inflearn_program_solving/String9.java
@@ -1,21 +1,24 @@
 package inflearn_program_solving;
 
 import java.util.*;
+
 public class String9 {
-    public String solution(String str){
-        String answer="YES";
-        str=str.toUpperCase();
-        int len=str.length();
-        for(int i=0; i<len/2; i++){
-            if(str.charAt(i)!=str.charAt(len-i-1)) answer="NO";
+
+    public static void solution(String input) {
+        final char[] chars = input.toCharArray();
+        int answer = 0;
+        for (char c : chars) {
+            if(48 <=c && c <= 57) {
+                answer = answer * 10 + (c - 48);
+            }
         }
-        return answer;
+
+        System.out.println(answer);
     }
 
-    public static void main(String[] args){
-        String9 T = new String9();
-        Scanner kb = new Scanner(System.in);
-        String str=kb.next();
-        System.out.print(T.solution(str));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        solution(input);
     }
 }
