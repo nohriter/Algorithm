d8d5eb6848e7518547f48a7ae5a03002525b746d
nori <nohriter@gmail.com>
2022-03-23 23:22:16 +0900

    자바 알고리즘 문제풀이 1-10

---
+++ src/inflearn_program_solving/String10.java
@@ -0,0 +1,24 @@
+package inflearn_program_solving;
+
+import java.util.Scanner;
+
+public class String10 {
+
+    public static void solution(String[] input) {
+        char[] c = input[0].toCharArray();
+        String find = input[1];
+        // teachermode
+        for (int i = 0; i < c.length; i++) {
+
+        }
+
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        final String input = sc.nextLine();
+        solution(input.split(" ").);
+    }
+}
