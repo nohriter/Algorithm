71584b033e4ce41709916173e8b45fdeaa6b1037
nori <nohriter@gmail.com>
2022-03-26 23:24:57 +0900

    자바 알고리즘 문제풀이 1-12

--- src/inflearn_program_solving/String11.java
+++ src/inflearn_program_solving/String11.java
@@ -6,33 +6,31 @@ import java.util.Scanner;
 public class String11 {
 
     public static void solution(String input) {
-        final char[] chars = input.toCharArray();
+        char[] chars = input.toCharArray();
 
-        final HashMap<Character, Integer> count = new HashMap<>();
-        String answer = "";
+        HashMap<Character, Integer> count = new HashMap<>();
+        String answer;
         //KKHSSSSSSSE
         count.put(chars[0], 1); // 넣고 값 + 1
+        StringBuilder answerBuilder = new StringBuilder();
         for (int i = 1; i < chars.length; i++) {
-            if(chars[i] == chars[i-1]) {
+            if (chars[i] == chars[i - 1]) {
                 count.put(chars[i], count.get(chars[i]) + 1); // 넣고 값 + 1
-            }else {
-                if(count.get(chars[i-1]) == 1) {
-                    answer += chars[i-1];
-                }else {
-                    answer += chars[i-1] +""+ count.get(chars[i-1]);
+            } else {
+                if (count.get(chars[i - 1]) == 1) {
+                    answerBuilder.append(chars[i - 1]);
+                } else {
+                    answerBuilder.append(chars[i - 1]).append(count.get(chars[i - 1]));
                 }
                 count.put(chars[i], 1);
             }
         }
-        if(count.get(chars[chars.length-1]) == 1) {
-            answer += chars[chars.length -1];
-        }else {
-            answer += chars[chars.length - 1] +""+ count.get(chars[chars.length - 1]);
-        }
-
+        answer = answerBuilder.toString();
+        if (count.get(chars[chars.length - 1]) == 1) {
+            answer += chars[chars.length - 1];
+        } else answer += chars[chars.length - 1] + "" + count.get(chars[chars.length - 1]);
 
         System.out.println(answer);
-        
     }
 
 
---
+++ src/inflearn_program_solving/String12.java
@@ -0,0 +1,20 @@
+package inflearn_program_solving;
+
+import java.util.Scanner;
+
+public class String12 {
+
+    // input
+    // 4
+    //#****###**#####**#####**##**
+
+    public static void solution(String input) {
+
+    }
+
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final String input = scanner.nextLine();
+        solution(input);
+    }
+}
