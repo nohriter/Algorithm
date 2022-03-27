6cd32e05e564f720ca6d43e378cf76e8a00b8866
nori <nohriter@gmail.com>
2022-03-27 22:52:42 +0900

    자바 알고리즘 문제풀이 1-12

--- src/inflearn_program_solving/String12.java
+++ src/inflearn_program_solving/String12.java
@@ -1,5 +1,8 @@
 package inflearn_program_solving;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class String12 {
@@ -7,14 +10,47 @@ public class String12 {
     // input
     // 4
     //#****###**#####**#####**##**
+    public static void solution(int N, String input) {
+        int start = 0;
+        int end = 7;
+        StringBuilder sb = new StringBuilder();
 
-    public static void solution(String input) {
+        for (int i = 0; i < N; i++) {
+            final String substring = input.substring(start, end);
+            final String twoNumber = parseTwo(substring);
+            start += 7;
+            end += 7;
+            sb.append((char) parseString(twoNumber).intValue());
+        }
 
+        System.out.println(sb);
+    }
+
+    private static Integer parseString(String twoNumber) {
+        return Integer.valueOf(twoNumber, 2);
+    }
+
+    private static String parseTwo(String secret) {
+        final String[] split = secret.split("");
+
+        StringBuilder two = new StringBuilder();
+
+        for (String s : split) {
+            if(s.equals("#")) {
+                two.append("1");
+            }else{
+                two.append("0");
+            }
+
+        }
+
+        return two.toString();
     }
 
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
+        final String N = scanner.nextLine();
         final String input = scanner.nextLine();
-        solution(input);
+        solution(Integer.parseInt(N), input);
     }
 }
