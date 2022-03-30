d0701b1d4e9d6a840c56dd4a1875021afa840c48
nori <nohriter@gmail.com>
2022-03-30 23:51:43 +0900

    자바 알고리즘 문제풀이 2-4

---
+++ src/inflearn_program_solving/array/Array4.java
@@ -0,0 +1,20 @@
+package inflearn_program_solving.array;
+
+import java.util.Scanner;
+
+public class Array4 {
+
+    public static void solution(int input) {
+        int temp = 0;
+
+        for (int i = 0; i < input; i++) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int input = scanner.nextInt();
+        solution(input);
+    }
+}
