b1f756428bed6b5f44c6ed624ab932a55a5baea8
nori <nohriter@gmail.com>
2022-04-05 23:57:16 +0900

    자바 알고리즘 문제풀이 2-8

--- src/inflearn_program_solving/array/Array8.java
+++ src/inflearn_program_solving/array/Array8.java
@@ -1,26 +1,29 @@
 package inflearn_program_solving.array;
 
-import java.util.*;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
 class Array8 {
-    public int[] solution(int n, int[] arr){
-        int[] answer = new int[n];
-        for(int i=0; i<n; i++){
-            int cnt=1;
-            for(int j=0; j<arr.length; j++){
-                if(arr[j]>arr[i]) cnt++;
+    public static int[] solution(int number, int[] scores) {
+        final int[] sorted = Arrays.stream(scores).sorted().toArray();
+        int[] answer = new int[number];
+
+        for (int i = 0; i < scores.length; i++) {
+            for (int j = 0; j < sorted.length; j++) {
+
             }
-            answer[i]=cnt;
         }
-        return answer;
     }
-    public static void main(String[] args){
-        Array8 T = new Array8();
-        Scanner kb = new Scanner(System.in);
-        int n=kb.nextInt();
-        int[] arr=new int[n];
-        for(int i=0; i<n; i++){
-            arr[i]=kb.nextInt();
+
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int number = Integer.parseInt(scanner.nextLine());
+        int[] scores = new int[number];
+        for (int i = 0; i < number; i++) {
+            scores[i] = scanner.nextInt();
         }
-        for(int x :T.solution(n, arr)) System.out.print(x+" ");
+        solution(number, scores);
     }
 }
