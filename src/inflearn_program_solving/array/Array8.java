4cdd1b086f5f40cfbca9ec2804364992e3b06c91
nori <nohriter@gmail.com>
2022-04-06 23:50:56 +0900

    자바 알고리즘 문제풀이 2-9

--- src/inflearn_program_solving/array/Array8.java
+++ src/inflearn_program_solving/array/Array8.java
@@ -1,26 +1,32 @@
 package inflearn_program_solving.array;
 
-
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
-import java.util.stream.IntStream;
 
 class Array8 {
-    public static int[] solution(int number, int[] scores) {
-        final int[] sorted = Arrays.stream(scores).sorted().toArray();
-        int[] answer = new int[number];
+    public static void solution(int number, Integer[] scores) {
+        final Integer[] sorted = Arrays.copyOf(scores, scores.length);
+        Arrays.sort(sorted, Collections.reverseOrder());
+        StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < scores.length; i++) {
-            for (int j = 0; j < sorted.length; j++) {
-
+        for (int i = 0; i < sorted.length; i++) {
+            for (int j = 0; j < scores.length; j++) {
+                if(sorted[i] == scores[j]) {
+                    scores[j] = i + 1;
+                }
             }
+        }
+
+        for (Integer score : scores) {
+            sb.append(score + " ");
         }
     }
 
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         final int number = Integer.parseInt(scanner.nextLine());
-        int[] scores = new int[number];
+        Integer[] scores = new Integer[number];
         for (int i = 0; i < number; i++) {
             scores[i] = scanner.nextInt();
         }
