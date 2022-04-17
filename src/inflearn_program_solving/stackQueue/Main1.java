b9b85f86db92c6d30ec2b7dcb71861d017291004
nori <nohriter@gmail.com>
2022-04-17 23:55:04 +0900

    자바 알고리즘 문제풀이 5-1

---
+++ src/inflearn_program_solving/stackQueue/Main1.java
@@ -0,0 +1,33 @@
+package inflearn_program_solving.stackQueue;
+
+import java.util.*;
+
+public class Main1 {
+
+    public static String solution(String input) {
+        String answer="YES";
+        Stack<Character> stack = new Stack<>();
+
+        for (char aChar : input.toCharArray()) {
+            if(aChar == '(') {
+                stack.push(aChar);
+            } else {
+                if(stack.isEmpty()) return "NO";
+                stack.pop();
+            }
+        }
+
+        if(!stack.isEmpty()) {
+            return "NO";
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        solution(input);
+    }
+
+}
