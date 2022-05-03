49bf285a9ad2c482b5f669589ec6976362dd3e3c
nori <nohriter@gmail.com>
2022-05-03 23:52:22 +0900

    자바 알고리즘 문제풀이 SortingAndSearching

---
+++ src/inflearn_program_solving/SortingAndSearching/Main5.java
@@ -0,0 +1,27 @@
+package inflearn_program_solving.SortingAndSearching;
+
+
+import java.util.*;
+
+public class Main5 {
+    public String solution(int n, int[] arr) {
+        String answer = "U";
+        Arrays.sort(arr);
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                answer = "D";
+                break;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main5 T = new Main5();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
+        System.out.println(T.solution(n, arr));
+    }
+}
