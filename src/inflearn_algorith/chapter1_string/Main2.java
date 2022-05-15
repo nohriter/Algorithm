f6f79313a6d416bb7208c3b6d4aa12e085984a8d
nori <nohriter@gmail.com>
2022-05-15 23:46:25 +0900

    인프런 알고리즘 문제풀이 chapter 1

---
+++ src/inflearn_algorith/chapter1_string/Main2.java
@@ -0,0 +1,23 @@
+package inflearn_algorith.chapter1_string;
+
+
+import java.util.*;
+
+public class Main2 {
+    public String solution(String str) {
+        String answer = "";
+        for (char x : str.toCharArray()) {
+            if (Character.isLowerCase(x)) answer += Character.toUpperCase(x);
+            else answer += Character.toLowerCase(x);
+
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main2 T = new Main2();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.print(T.solution(str));
+    }
+}
