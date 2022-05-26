7fd80d8f6322beef967de8cf313501871790f326
nori <nohriter@gmail.com>
2022-05-26 23:54:27 +0900

    인프런 알고리즘 문제풀이 chapter 1

---
+++ src/inflearn_algorithm/chapter1_string/Main10.java
@@ -0,0 +1,39 @@
+package inflearn_algorithm.chapter1_string;
+
+import java.util.*;
+
+public class Main10 {
+
+    public int[] solution(String s, char t) {
+        int[] answer = new int[s.length()];
+        int p = 1000;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == t) {
+                p = 0;
+                answer[i] = p;
+            } else {
+                p++;
+                answer[i] = p;
+            }
+        }
+        p = 1000;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) == t) p = 0;
+            else {
+                p++;
+                answer[i] = Math.min(answer[i], p);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main10 T = new Main10();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        char c = kb.next().charAt(0);
+        for (int x : T.solution(str, c)) {
+            System.out.print(x + " ");
+        }
+    }
+}
