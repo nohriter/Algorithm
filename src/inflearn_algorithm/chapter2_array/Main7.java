28bc8e00387785d72b75970b271de3420e053970
nori <nohriter@gmail.com>
2022-06-09 22:42:36 +0900

    인프런 알고리즘 문제풀이 chapter 2

---
+++ src/inflearn_algorithm/chapter2_array/Main7.java
@@ -0,0 +1,27 @@
+package inflearn_algorithm.chapter2_array;
+
+import java.util.Scanner;
+
+public class Main7 {
+    public int solution(int n, int[] arr) {
+        int answer = 0, cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 1) {
+                cnt++;
+                answer += cnt;
+            } else cnt = 0;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main7 T = new Main7();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        System.out.print(T.solution(n, arr));
+    }
+}
