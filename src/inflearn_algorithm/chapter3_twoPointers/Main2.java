11107b2d85a46b300970294c0077bbbc721886ae
nori <nohriter@gmail.com>
2022-06-29 23:55:36 +0900

    인프런 알고리즘 문제풀이 chapter 3

---
+++ src/inflearn_algorithm/chapter3_twoPointers/Main2.java
@@ -0,0 +1,47 @@
+package inflearn_algorithm.chapter3_twoPointers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main2 {
+
+    private ArrayList<Integer> solution(int n, int m, int[] a, int[] b) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        int p1 = 0;
+        int p2 = 0;
+        while (p1 < n && p2 < m) {
+            if (a[p1] == b[p2]) {
+                answer.add(a[p1++]);
+                p2++;
+            }
+            else if (a[p1] < b[p2]) p1++;
+            else p2++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main2 T = new Main2();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        int m = sc.nextInt();
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++) {
+            b[i] = sc.nextInt();
+        }
+
+        for (int x : T.solution(n, m, a, b)) {
+            System.out.println(x + " ");
+        }
+    }
+
+}
