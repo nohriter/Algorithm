017a11b1950b14d5c03f839b8223a840deda08f8
nori <nohriter@gmail.com>
2022-07-03 23:48:35 +0900

    인프런 알고리즘 문제풀이 chapter 3

---
+++ src/inflearn_algorithm/chapter3_twoPointers/Main5.java
@@ -0,0 +1,24 @@
+package inflearn_algorithm.chapter3_twoPointers;
+
+import java.util.*;
+
+public class Main5 {
+
+    public int solution(int n) {
+        int answer = 0, cnt = 1;
+        n--;
+        while (n > 0) {
+            cnt++;
+            n = n - cnt;
+            if (n % cnt == 0) answer++;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main5 T = new Main5();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        System.out.print(T.solution(n));
+    }
+}
