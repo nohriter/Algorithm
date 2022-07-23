807a2db383d98fa241bc135367aa5996f542915b
nori <nohriter@gmail.com>
2022-07-23 23:38:58 +0900

    Doit 알고리즘 코딩테스트

---
+++ src/algorithm_codingtest/basic/data_structure/chapter1_배열과리스트/P1546.java
@@ -0,0 +1,29 @@
+package algorithm_codingtest.basic.data_structure.chapter1_배열과리스트;
+
+import java.util.Scanner;
+
+public class P1546 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] scores = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            scores[i] = sc.nextInt();
+        }
+
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(max < scores[i]) {
+                max = scores[i];
+            }
+            sum += scores[i];
+        }
+
+        System.out.println(sum * 100.0 / max / n);
+    }
+}
