e2abd70e7965eb6698acfb3d89de95a64777875e
nori <nohriter@gmail.com>
2022-07-26 23:53:23 +0900

    인프런 알고리즘 문제풀이 chapter 6 삽입정렬

---
+++ src/inflearn_algorithm/chapter6_sort/Main3.java
@@ -0,0 +1,28 @@
+package inflearn_algorithm.chapter6_sort;
+
+import java.util.Scanner;
+
+
+public class Main3 {
+
+    public int[] solution(int n, int[] arr) {
+        for (int i = 1; i < n; i++) {
+            int tmp = arr[i], j;
+            for (j = i - 1; j >= 0; j--) {
+                if (arr[j] > tmp) arr[j + 1] = arr[j];
+                else break;
+            }
+            arr[j + 1] = tmp;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Main3 T = new Main3();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
+        for (int x : T.solution(n, arr)) System.out.print(x + " ");
+    }
+}
