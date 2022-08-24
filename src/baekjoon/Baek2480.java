f8ccab58aeb89a2480f39370b4af6016637b33fa
nori <nohriter@gmail.com>
2022-08-24 23:29:48 +0900

    백준 2480

--- src/baekjoon/Baek2480.java
+++ src/baekjoon/Baek2480.java
@@ -6,16 +6,36 @@ public class Baek2480 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int count = 0;
 
-        for (int i = 0; i < 3; i++) {
-            if(sc.nextInt() == a) {
-                count++;
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int answer = 0;
+
+        if (a == b && a == c) {
+            answer = 10000 + a * 1000;
+        } else if (a == b) {
+            answer = 1000 + a * 100;
+        } else if (b == c) {
+            answer = 1000 + b * 100;
+        } else if (a == c) {
+            answer = 1000 + a * 100;
+        } else {
+            int max = a;
+            if(b > a) {
+                max = b;
+                if(c > b) {
+                    max = c;
+                }
+            }else if(c > a) {
+                max = c;
             }
+
+           answer = max * 100;
         }
 
-
+        System.out.println(answer);
     }
 
 }
