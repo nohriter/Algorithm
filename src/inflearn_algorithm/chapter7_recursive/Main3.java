855e960c86b3819fbde2d75f24b75b1dba4f4696
nori <nohriter@gmail.com>
2022-08-29 22:04:39 +0900

    인프런 알고리즘 팩토리얼

---
+++ src/inflearn_algorithm/chapter7_recursive/Main3.java
@@ -0,0 +1,14 @@
+package inflearn_algorithm.chapter7_recursive;
+
+public class Main3 {
+
+    public int DFS(int n) {
+        if (n == 1) return 1;
+        else return n * DFS(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Main3 T = new Main3();
+        System.out.println(T.DFS(5));
+    }
+}
