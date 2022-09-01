f8efdb96da64aa998b6257e1f90622df95aacb9b
nori <nohriter@gmail.com>
2022-09-01 11:04:11 +0900

    백준 10828

--- src/baekjoon/Baek10828.java
+++ src/baekjoon/Baek10828.java
@@ -1,89 +1,38 @@
 package baekjoon;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Baek10828 {
 
-    public static int[] stack;
-    public static int size = 0;
+    static Stack<String> stack = new Stack<>();
 
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
 
-        int N = in.nextInt();
+        int N = Integer.parseInt(sc.nextLine());
 
-        stack = new int[N];
+        for (int i = 0; i < N; i++) {
+            String str = sc.nextLine();
 
-        for(int i = 0; i < N; i++) {
-
-            String str = in.next();
-
-            switch (str) {
-
-                case "push":
-                    push(in.nextInt());
-                    break;
-
-                case "pop":
-                    sb.append(pop()).append('\n');
-                    break;
-
-                case "size":
-                    sb.append(size()).append('\n');
-                    break;
-
-                case "empty":
-                    sb.append(empty()).append('\n');
-                    break;
-
-                case "top":
-                    sb.append(top()).append('\n');
-                    break;
+            if (str.startsWith("push")) {
+                stack.push(str.split(" ")[1]);
+            } else if (str.equals("top")) {
+                if (stack.isEmpty()) sb.append("-1\n");
+                else sb.append(stack.peek()+"\n");
+            } else if (str.equals("size")) {
+                sb.append(stack.size()+ "\n");
+            } else if (str.equals("empty")) {
+                if (stack.isEmpty()) sb.append("1\n");
+                else sb.append("0\n");
+            } else if (str.equals("pop")) {
+                if (stack.isEmpty()) sb.append("-1\n");
+                else sb.append(stack.pop()+"\n");
             }
-
         }
         System.out.println(sb);
-    }
-
-    public static void push(int item) {
-        stack[size] = item;
-        size++;
-    }
-
-    public static int pop() {
-        if(size == 0) {
-            return -1;
-        }
-        else {
-            int res = stack[size - 1];
-            stack[size - 1] = 0;
-            size--;
-            return res;
-        }
-    }
-
-    public static int size() {
-        return size;
-    }
-
-    public static int empty() {
-        if(size == 0) {
-            return 1;
-        }
-        else {
-            return 0;
-        }
-    }
-
-    public static int top() {
-        if(size == 0) {
-            return -1;
-        }
-        else {
-            return stack[size - 1];
-        }
     }
 
 }
