580f4621f49b3e4dd338d6e6e87aebec480ea14f
nori <nohriter@gmail.com>
2022-09-02 19:38:36 +0900

    백준 1152

---
+++ src/baekjoon/Baek1152.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baek1152 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        if (s.isBlank()) {
+            System.out.println("0");
+        } else {
+            String[] split = s.trim().split(" ");
+            System.out.println(split.length);
+        }
+
+
+    }
+}
+
+//    Scanner str = new Scanner(System.in);
+//
+//    String s = str.nextLine();
+//		str.close();
+//
+//                  StringTokenizer st=new StringTokenizer(s," ");
+//
+//                System.out.println(st.countTokens());
