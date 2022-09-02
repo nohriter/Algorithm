62a2ad390687a483bf5ee446bfaee4da1a831544
nori <nohriter@gmail.com>
2022-09-02 16:31:16 +0900

    백준 1158 요세푸스

--- src/baekjoon/Baek1158.java
+++ src/baekjoon/Baek1158.java
@@ -1,39 +1,34 @@
 package baekjoon;
 
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Baek1158 {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-
-        Queue<Integer> q = new LinkedList<>();
-        for (int i = 1; i <= n; i++)
-            q.offer(i);
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
 
         StringBuilder sb = new StringBuilder();
         sb.append("<");
 
-        int cnt = 0;
-        while (!q.isEmpty()) {
-            cnt++;
-            if (cnt % k == 0)
-                sb.append(q.poll() + ", ");
-            else
-                q.offer(q.poll());
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= N; i++) {
+            queue.offer(i);
         }
 
-        sb.setLength(sb.length() - 2);
-        sb.append(">");
+        while(queue.size() > 1) {
+            for (int i = 0; i < K -1; i++) {
+                queue.offer(queue.poll());
+            }
+            sb.append(queue.poll()).append(", ");
+        }
+        sb.append(queue.poll()).append(">");
+
         System.out.println(sb);
     }
 }
