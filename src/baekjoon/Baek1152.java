8ea1cb5d49afcf6ffde2863ee1571e6c827199b8
nori <nohriter@gmail.com>
2022-09-02 19:41:30 +0900

    백준 1152

--- src/baekjoon/Baek1152.java
+++ src/baekjoon/Baek1152.java
@@ -7,6 +7,7 @@ public class Baek1152 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+
         String s = sc.nextLine();
         if (s.isBlank()) {
             System.out.println("0");
