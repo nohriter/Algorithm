c1af69f1899f50a2418adfe59d2abf0e36ca7823
nori <nohriter@gmail.com>
2022-09-02 17:21:01 +0900

    백준 10866 덱

--- src/baekjoon/Baek10866.java
+++ src/baekjoon/Baek10866.java
@@ -1,87 +1,57 @@
 package baekjoon;
 
-
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class Baek10866 {
+
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        ArrayDeque<Integer> dq = new ArrayDeque<Integer>();
-        StringBuilder sb = new StringBuilder();
-
         int N = Integer.parseInt(br.readLine());
 
+        StringBuilder sb = new StringBuilder();
+
+        Deque<Integer> deque = new ArrayDeque<>();
+
+        StringTokenizer st;
+
         for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
 
-            String[] s = br.readLine().split(" ");
-
-            // 첫 번째 단어에 대한 switch-case
-            switch (s[0]) {
-
-                case "push_front": {
-                    dq.addFirst(Integer.parseInt(s[1]));
+            switch (st.nextToken()) {
+                case "push_front":
+                    deque.addFirst(Integer.parseInt(st.nextToken()));
                     break;
-                }
-
-                case "push_back": {
-                    dq.addLast(Integer.parseInt(s[1]));
+                case "push_back":
+                    deque.addLast(Integer.parseInt(st.nextToken()));
                     break;
-                }
-
-                case "pop_front": {
-                    if (dq.isEmpty()) {
-                        sb.append(-1).append('\n');
-                    } else {
-                        sb.append(dq.pollFirst()).append('\n');
-                    }
+                case "pop_front":
+                    if (deque.isEmpty()) sb.append("-1").append("\n");
+                    else sb.append(deque.pollFirst()).append("\n");
                     break;
-                }
-
-                case "pop_back": {
-                    if (dq.isEmpty()) {
-                        sb.append(-1).append('\n');
-                    } else {
-                        sb.append(dq.pollLast()).append('\n');
-                    }
+                case "pop_back":
+                    if (deque.isEmpty()) sb.append("-1").append("\n");
+                    else sb.append(deque.pollLast()).append("\n");
                     break;
-                }
-
-                case "size": {
-                    sb.append(dq.size()).append('\n');
+                case "size":
+                    sb.append(deque.size()).append("\n");
                     break;
-                }
-
-                case "empty": {
-                    if (dq.isEmpty()) {
-                        sb.append(1).append('\n');
-                    } else {
-                        sb.append(0).append('\n');
-                    }
+                case "empty":
+                    if (deque.isEmpty()) sb.append("1").append("\n");
+                    else sb.append("0").append("\n");
                     break;
-                }
-
-                case "front": {
-                    if (dq.isEmpty()) {
-                        sb.append(-1).append('\n');
-                    } else {
-                        sb.append(dq.peekFirst()).append('\n');
-                    }
+                case "front":
+                    if (deque.isEmpty()) sb.append("-1").append("\n");
+                    else sb.append(deque.peekFirst()).append("\n");
                     break;
-                }
-
-                case "back": {
-                    if (dq.isEmpty()) {
-                        sb.append(-1).append('\n');
-                    } else {
-                        sb.append(dq.peekLast()).append('\n');
-                    }
+                case "back":
+                    if (deque.isEmpty()) sb.append("-1").append("\n");
+                    else sb.append(deque.peekLast()).append("\n");
                     break;
-                }
             }
         }
         System.out.println(sb);
