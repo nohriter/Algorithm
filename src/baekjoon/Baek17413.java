03dee87354ddd407bbd6dd4d14ece0e8a5e1ead8
nori <nohriter@gmail.com>
2022-09-04 23:56:21 +0900

    백준 17413

--- src/baekjoon/Baek17413.java
+++ src/baekjoon/Baek17413.java
@@ -5,7 +5,6 @@ import java.util.Stack;
 
 public class Baek17413 {
 
-
     static void print(Stack st) {
         while (!st.empty()) {
             System.out.print(st.peek());
@@ -44,6 +43,5 @@ public class Baek17413 {
         print(st);
 
     }
-
 
 }
