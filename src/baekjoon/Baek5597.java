3cbce1288c41cf845e416b0bfef6866712e7addf
nori <nohriter@gmail.com>
2022-09-07 22:11:15 +0900

    백준 5597

--- src/baekjoon/Baek5597.java
+++ src/baekjoon/Baek5597.java
@@ -4,26 +4,20 @@ import java.util.Scanner;
 
 public class Baek5597 {
 
-        public static void main(String[] args) {
-            Scanner sc = new Scanner(System.in);
-            int[] student = new int[31];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] student = new int[31];
 
-            for(int i=1; i<29; i++) {
-                int success = sc.nextInt();
-                student[success] = 1;
-            }
-            for(int i=1; i<student.length; i++) {
-                if(student[i]!=1)
-                    System.out.println(i);
-            }
-
-
-            sc.close();
-
+        for (int i = 1; i <= 28; i++) {
+            int success = sc.nextInt();
+            student[success] = 1;
+        }
+        for (int i = 1; i < student.length; i++) {
+            if (student[i] != 1)
+                System.out.println(i);
         }
 
-
-
-
+        sc.close();
+    }
 
 }
