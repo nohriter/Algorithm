67989d8f479ee8d7a19c3dd4021a4bf8b7dbaf49
nori <nohriter@gmail.com>
2022-09-08 08:15:41 +0900

    백준 2743

---
+++ src/baekjoon/Baek2743.java
@@ -0,0 +1,10 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Baek2743 {
+
+    public static void main(String[] args) {
+        System.out.println(new Scanner(System.in).nextLine().length());
+    }
+}
