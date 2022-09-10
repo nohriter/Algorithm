d7f3bf5cfb66ed42c5f61bb9fbaf6e28402cadd2
nori <nohriter@gmail.com>
2022-09-10 09:49:52 +0900

    백준 11718

---
+++ src/baekjoon/Baek11718.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Baek11718 {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            System.out.println(sc.nextLine());
+        }
+
+    }
+}
